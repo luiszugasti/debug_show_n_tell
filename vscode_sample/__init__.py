159da3c05ec952f68958c8847741559bc71cda3a
luiszugasti <luis.zugasti@outlook.com>
2021-02-16 17:55:49 -0500

    introduced some silly bugs

--- vscode_sample/__init__.py
+++ vscode_sample/__init__.py
@@ -3,12 +3,12 @@ import math
 
 # globals
 __version__ = "0.1.0"
-TOLERANT_DIVERGENCE = 0.0000001
+TOLERANT_DIVERGENCE = 0.0001
 
 
 def get_square_root(a, x):
     """ Recursively computes the square root """
-    new_x = (x + a / x) / 2
+    new_x = (x + a / x) // 2
     if math.fabs(x - new_x) < TOLERANT_DIVERGENCE:
         return new_x
 
@@ -20,19 +20,18 @@ def get_euclidian_distance(row1, row2):
 
     # Consider, for a moment, that we didn't know _what_was happening with our program.
     # we could try print statements.
-    # print("row1's x entry: {}, row1's y entry: {}".format(row1["x"], row1["y"]))
+    print("row1's x entry: {}, row1's y entry: {}".format(row1["x"], row1["y"]))
 
     # print statements are potentially brittle, depending on how much they expect from a data structure/API
-    # print("row2's x entry: {}, row2's y entry: {}".format(row1["X"], row1["Y"]))
+    print("row2's x entry: {}, row2's y entry: {}".format(row1["X"], row1["Y"]))
 
-    # what are we doing wrong here?
-    # intermediate_y = row1["y"] - row2["y"]
-    # intermediate_x = row1["x"] - row2["x"]
+    intermediate_y = row1["y"] ** 2 - row2["y"] ** 2
+    intermediate_x = row1["x"] ** 2 - row2["x"] ** 2
 
-    intermediate_y = math.fabs(int(row1["y"]) - int(row2["y"]))
-    intermediate_x = math.fabs(int(row1["x"]) - int(row2["x"]))
+    intermediate_y = int(row1["y"]) - int(row2["y"])
+    intermediate_x = int(row1["x"]) - int(row2["x"])
 
-    return get_square_root(intermediate_x ** 2 + intermediate_y ** 2, 1.0)
+    return get_square_root(intermediate_x + intermediate_y, 1.0)
 
 
 def create_2d_point(x, y):
